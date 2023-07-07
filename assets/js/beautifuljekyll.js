0f386c2dd0de84e95dc3efad14a0ec8c38bd7eae
Rishabh <81488614+borlaugg@users.noreply.github.com>
2023-07-07 15:52:50 +0530

    Update beautifuljekyll.js

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -135,6 +135,30 @@ let BeautifulJekyllJS = {
       }
     });
   }
+
+  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+  
+  function switchTheme(e) {
+      if (e.target.checked) {
+          document.documentElement.setAttribute('data-theme', 'dark');
+      }
+      else {
+          document.documentElement.setAttribute('data-theme', 'light');
+      }    
+  }
+  
+  toggleSwitch.addEventListener('change', switchTheme, false);
+
+  const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+  
+  if (currentTheme) {
+      document.documentElement.setAttribute('data-theme', currentTheme);
+  
+      if (currentTheme === 'dark') {
+          toggleSwitch.checked = true;
+      }
+  }
+
 };
 
 // 2fc73a3a967e97599c9763d05e564189
